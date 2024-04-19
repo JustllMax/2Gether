518fbf1ea8797f38338c14284e3f27a4e88933f5
kubs <mctest2334@gmail.com>
2024-04-19 12:06:38 +0200

    remove walk check

--- Assets/_Source/Player/PlayerController.cs
+++ Assets/_Source/Player/PlayerController.cs
@@ -173,16 +173,13 @@ public class PlayerController : MonoBehaviour
     {
         if (_dashCount > 0 && !_isDashing)
         {
-            Dash();
+            _ = Dash();
             _dashCount--;
         }
     }
 
     private async UniTaskVoid Dash()
     {
-        if (!_isMoving)
-            return;
-
         _isDashing = true;
 
         Vector3 movement = _lastMovementDir;
