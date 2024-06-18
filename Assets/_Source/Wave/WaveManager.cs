ea3cfa941d65bd33cbfe8004bc60bcab5b934fd1
Ruslan <s093803@student.tu.kielce.pl>
2024-06-19 00:01:59 +0200

    -

--- Assets/_Source/Wave/WaveManager.cs
+++ Assets/_Source/Wave/WaveManager.cs
@@ -56,9 +56,6 @@ public class WaveManager : MonoBehaviour
             waves.Add(new SingleWave(new List<GameObject>(wave.Enemies), wave.EnemySpawnInterval, wave.Cooldown));
             waveClearExpected += wave.Cooldown;
         }
-
-        Debug.LogErrorFormat("Generated follow elements: " + string.Join(", ", _weightsFollow));
-        Debug.LogErrorFormat("Generated attack elements: " + string.Join(", ", _weightsAttack));
     }
     void OnEnable()
     {
