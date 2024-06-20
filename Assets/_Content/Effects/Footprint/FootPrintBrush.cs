d440fc387fad1b589342a4ef97bc20689ab17245
Ruslan <s093803@student.tu.kielce.pl>
2024-06-20 17:43:50 +0200

    Footprint shader

--- Assets/_Content/Effects/Footprint/FootPrintBrush.cs
+++ Assets/_Content/Effects/Footprint/FootPrintBrush.cs
@@ -5,50 +5,22 @@ using UnityEngine;
 public class FootPrintBrush : MonoBehaviour
 {
     public CustomRenderTexture HeightMap;
-    public Texture2D frame;
     public Material HeightMapUpdate;
 
-    public float SecondsToRestore = 100;
-
-    public GameObject[] Tires;
-
     private Camera mainCamera;
-    private int tireIndex;
 
-    private float timeToRestoreOneTick;
-    
     private static readonly int DrawPosition = Shader.PropertyToID("_DrawPosition");
     private static readonly int DrawAngle = Shader.PropertyToID("_DrawAngle");
-    private static readonly int RestoreAmount = Shader.PropertyToID("_RestoreAmount");
 
     private void Start()
     {
         HeightMap.Initialize();
         mainCamera = Camera.main;
-
-
     }
 
     private void Update()
     {
-        // Раскомментируйте одну из этих строчек, чтобы выбрать какие объекты будут копать снег
         DrawWithMouse();
-        DrawWithTires();
-
-        // Считаем таймер до восстановления каждого пикселя текстуры на единичку 
-        timeToRestoreOneTick -= Time.deltaTime;
-        if (timeToRestoreOneTick < 0)
-        {
-            // Если в этот update мы хотим увеличить цвет всех пикселей карты высот на 1
-            HeightMapUpdate.SetFloat(RestoreAmount, 1 / 250f);
-            timeToRestoreOneTick = SecondsToRestore / 250f;
-        }
-        else
-        {
-            // Если не хотим
-            HeightMapUpdate.SetFloat(RestoreAmount, 0);
-        }
-        
         HeightMap.Update();
     }
 
@@ -68,18 +40,20 @@ public class FootPrintBrush : MonoBehaviour
         }
     }
 
-    private void DrawWithTires()
+    void OnTriggerStay(Collider other)
     {
-        GameObject tire = Tires[tireIndex++ % Tires.Length];
-
-        Ray ray = new Ray(tire.transform.position, Vector3.down);
-        if (Physics.Raycast(ray, out RaycastHit hit))
+        Debug.Log("collision " + other.gameObject.name);
+        if (other.CompareTag("Player") || other.CompareTag("Enemy"))
         {
-            Vector2 hitTextureCoord = hit.textureCoord;
-            float angle = tire.transform.rotation.eulerAngles.y;
+            Ray ray = new Ray(other.transform.position, Vector3.down);
+            if (Physics.Raycast(ray, out RaycastHit hit))
+            {
+                Vector2 hitTextureCoord = hit.textureCoord;
+                float angle = other.transform.rotation.eulerAngles.y;
 
-            HeightMapUpdate.SetVector(DrawPosition, hitTextureCoord);
-            HeightMapUpdate.SetFloat(DrawAngle, angle * Mathf.Deg2Rad);
+                HeightMapUpdate.SetVector(DrawPosition, hitTextureCoord);
+                HeightMapUpdate.SetFloat(DrawAngle, angle * Mathf.Deg2Rad);
+            }
         }
     }
 }
